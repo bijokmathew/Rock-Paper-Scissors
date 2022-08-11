4d79952f38ed44e7e31812f57570508e22ff9364
bijokmathew <bijo.nbr@hotmail.com>
2022-08-11 15:53:02 +0000

    define generateComputerChoice and setComputerChoiceImage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,34 +7,37 @@ const resultMessage = document.getElementById('result-msg');
 const userChoiceImage = document.getElementById('user-image');
 const computerChoiceImage = document.getElementById('computer-image')
 // array for getting the computer selected image based on the random number
-const computerChoice = ["rock","paper","scissors"];
+const computerChoice = ["rock", "paper", "scissor"];
 // user and pc score 
 const userScore = document.getElementById('user-score');
 const computerScore = document.getElementById('computer-score')
 
 /* Register event listener for all buttons after loading the DOM  */
-document.addEventListener('DOMContentLoaded',function(){
-    for(let button of controlButtons){
-        button.addEventListener('click',function(){
+document.addEventListener('DOMContentLoaded', function () {
+    for (let button of controlButtons) {
+        button.addEventListener('click', function () {
             let buttonType = this.getAttribute('data-type');
-            if(buttonType=== "reset"){
+            if (buttonType === "reset") {
                 resetGame();
-            } else{
+            } else {
                 runGame(buttonType)
             }
         })
-    } 
+    }
 })
 /**
- * Reset the game by clearing the score and
+ * Reset the game by clearing the score ,set default(empty-image) to
+ * both user choice and computer choice images
  * go to the intial state of the game
  */
-function resetGame(){
-    //set the user and computer score to zero
-    userScore.innerHTML="0";
-    computerScore.innerText="0";
-    userChoiceImage.src=`assets/images/no-image.png`;
-    computerChoiceImage.src=`assets/images/no-image.png`;
+function resetGame() {
+    //set the user and computer score to zero and respective images
+    userScore.innerHTML = "0";
+    computerScore.innerText = "0";
+    userChoiceImage.src = `assets/images/no-image.png`;
+    userChoiceImage.alt = "empty image";
+    computerChoiceImage.src = `assets/images/no-image.png`;
+    computerChoiceImage.alt = "empty image";
 }
 /**
  *This is main function which takes buttonType as a parameter
@@ -42,7 +45,7 @@ function resetGame(){
  * based on the button type continue the game 
  * @param {*} buttonType 
  */
-function runGame(buttonType){
+function runGame(buttonType) {
     setUserChoiceImage(buttonType);
     generateComputerChoice();
 }
@@ -51,22 +54,25 @@ function runGame(buttonType){
  * And set the image on the HTML based on the buttonType
  * @param {*} buttonType 
  */
-function setUserChoiceImage(buttonType){
-    userChoiceImage.src=`assets/images/${buttonType}.png`
+function setUserChoiceImage(buttonType) {
+    userChoiceImage.src = `assets/images/${buttonType}.png`
+    userChoiceImage.alt = buttonType;
 }
 /**
  * This function set the image on the HTML 
  * based on the computer choice
+ * @param {*} ComputerRandomChoice
  */
- function setComputerChoiceImage(){
-
+function setComputerChoiceImage(ComputerRandomChoice) {
+    computerChoiceImage.src = `assets/images/` + computerChoice[ComputerRandomChoice] + `.png`;
+    computerChoiceImage.alt = computerChoice[ComputerRandomChoice];
 }
 /**
  * This function generate the computer choice
- * based on the random number generate between the 0 and userchoice length
+ * based on the random number generate between the 0 and computer choice array length
  * 
  */
-function generateComputerChoice(){
-
-
+function generateComputerChoice() {
+    let ComputerRandomChoice = Math.floor(Math.random() * computerChoice.length);
+    setComputerChoiceImage(ComputerRandomChoice);
 }
