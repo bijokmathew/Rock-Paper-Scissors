9d1e904bf132abfd3bd7b9fd5bd571f9f8835841
bijokmathew <bijo.nbr@hotmail.com>
2022-08-11 15:11:38 +0000

    decalre setUserChoiceImage,setComputerChoiceImage, generateComputerChoice and define resetGame functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 /* Declare constants for DOM elements */
 const controlButtons = document.getElementsByTagName('button');
 const resetButton = document.getElementById('reset-btn');
+//result message pop up
+const resultMessage = document.getElementById('result-msg');
+// user and pc selection image display
+const userChoiceImage = document.getElementById('user-image');
+const computerChoiceImage = document.getElementById('computer-image')
+// array for getting the computer selected image based on the random number
+const computerChoice = ["rock","paper","scissors"];
+// user and pc score 
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score')
 
 /* Register event listener for all buttons after loading the DOM  */
 document.addEventListener('DOMContentLoaded',function(){
     for(let button of controlButtons){
-        button.addEventListener("click",function(){
-            let buttonType = this.getAttribute("data-type");
+        button.addEventListener('click',function(){
+            let buttonType = this.getAttribute('data-type');
             if(buttonType=== "reset"){
                 resetGame();
             } else{
@@ -20,7 +30,9 @@ document.addEventListener('DOMContentLoaded',function(){
  * go to the intial state of the game
  */
 function resetGame(){
-
+    //set the user and computer score to zero
+    userScore.innerHTML="0";
+    computerScore.innerText="0";
 }
 /**
  *This is main function which takes buttonType as a parameter
@@ -29,5 +41,30 @@ function resetGame(){
  * @param {*} buttonType 
  */
 function runGame(buttonType){
+    setUserChoiceImage(buttonType);
+    generateComputerChoice();
+}
+/**
+ * This function takes buttonType as a parameter
+ * And set the image on the HTML based on the buttonType
+ * @param {*} buttonType 
+ */
+function setUserChoiceImage(buttonType){
+
+}
+/**
+ * This function set the image on the HTML 
+ * based on the computer choice
+ */
+ function setComputerChoiceImage(){
+
+}
+/**
+ * This function generate the computer choice
+ * based on the random number generate between the 0 and userchoice length
+ * 
+ */
+function generateComputerChoice(){
+
 
 }
